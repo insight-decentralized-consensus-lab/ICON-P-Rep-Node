3e404ee15282a060e36f665c0f1d1be5f7bb5320
robcxyz <rob@robot.localdomain>
2019-06-20 00:57:40 -0700

    imporved local key import

--- modules/keys/main.tf
+++ modules/keys/main.tf
@@ -13,13 +13,13 @@ locals {
 // TODO: This needs to have some logic to import existing keys and export to anywhere. Right now we just do both and
 //then import when whatever is left.
 resource "tls_private_key" "key" {
-//  count = "${var.local_key_file == "" ? 0 : 1}"
+//  count = "${var.local_public_key == "" ? 0 : 1}"
 
   algorithm = "RSA"
 }
 
 resource "local_file" "key_priv" {
-//  count = "${var.local_key_file == "" ? 0 : 1}"
+//  count = "${var.local_public_key == "" ? 0 : 1}"
 
   content  = "${tls_private_key.key.private_key_pem}"
   filename = "${path.module}/id_rsa"
@@ -27,7 +27,7 @@ resource "local_file" "key_priv" {
 
 
 resource "null_resource" "key_chown" {
-//  count = "${var.local_key_file == "" ? 0 : 1}"
+//  count = "${var.local_public_key == "" ? 0 : 1}"
 
   provisioner "local-exec" {
     command = "chmod 400 ${path.module}/id_rsa"
@@ -41,7 +41,7 @@ resource "null_resource" "key_chown" {
 
 
 resource "null_resource" "key_gen" {
-//  count = "${var.local_key_file == "" ? 0 : 1}"
+//  count = "${var.local_public_key == "" ? 0 : 1}"
 
   provisioner "local-exec" {
     command = "ssh-keygen -y -f ${path.module}/id_rsa > ${path.module}/id_rsa.pub"
@@ -54,18 +54,17 @@ resource "null_resource" "key_gen" {
 }
 
 data "local_file" "key_gen" {
-//  count = "${var.local_key_file == "" ? 0 : 1}"
+//  count = "${var.local_public_key == "" ? 0 : 1}"
   filename = "${path.module}/id_rsa.pub"
-
   depends_on = ["null_resource.key_gen"]
 }
 
 data "local_file" "key_local" {
-  count = "${var.local_key_file == "" ? 0 : 1}"
-  filename = "${var.local_key_file}"
+  count = "${var.local_public_key == "" ? 0 : 1}"
+  filename = "${var.local_public_key}"
 }
 
 resource "aws_key_pair" "key" {
   key_name = "${local.name}"
-  public_key = "${var.local_key_file == "" ? data.local_file.key_local.content : data.local_file.key_local.content}"
+  public_key = "${var.local_public_key == "" ? data.local_file.key_gen.content : data.local_file.key_local.content}"
 }
--- modules/keys/outputs.tf
+++ modules/keys/outputs.tf
@@ -1,3 +1,7 @@
 output "key_name" {
   value = "${aws_key_pair.key.key_name}"
 }
+
+output "public_key" {
+  value = "${var.local_public_key == "" ? aws_key_pair.key.public_key : data.local_file.key_local.content}"
+}
